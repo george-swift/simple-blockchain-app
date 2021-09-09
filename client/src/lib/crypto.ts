beafb11306f3810dd4ba4380431181350e062bf4
George <71434803+george-swift@users.noreply.github.com>
2021-09-09 16:43:49 +0100

    Add components to client side

---
+++ client/src/lib/crypto.ts
@@ -0,0 +1,7 @@
+export default async function sha256(data: string): Promise<string> {
+  const msgUint8 = new TextEncoder().encode(data);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+  return hashHex;
+}
